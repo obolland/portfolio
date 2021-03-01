28850dad89e52f7df4f893922e8400f7fca60f17
OllyB <info@ollybolland.com>
2021-03-01 09:19:03 +0000

    .

--- components/layouts/BaseLayout.js
+++ components/layouts/BaseLayout.js
@@ -1,6 +1,6 @@
 import dynamic from 'next/dynamic'
-import Header from '../shared/Header';
-// const Header = dynamic(() => import('../shared/Header'), {ssr: false});
+// import Header from '../shared/Header';
+const Header = dynamic(() => import('../shared/Header'), {ssr: false});
 import { ToastContainer } from 'react-toastify';
 
 const BaseLayout = ({ className, navClass="with-bg", children, user, userLoading }) => {
