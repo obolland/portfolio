ad0684ef4d1267e9258fb63ee55bc19d06d0196b
OllyB <info@ollybolland.com>
2021-03-01 10:58:19 +0000

    updated auth0 utils for production

--- utils/auth0.js
+++ utils/auth0.js
@@ -1,3 +1,11 @@
+if (process.env.NODE_ENV === 'production') {
+  const redirectUrl = 'https://portfolio-me-tau.vercel.app/api/v1/callback'
+  const postLogoutRedirectUrl = 'https://portfolio-me-tau.vercel.app/'
+} else {
+  const redirectUrl = 'http://localhost:3000/api/v1/callback'
+  const postLogoutRedirectUrl = 'http://localhost:3000/'
+}
+
 import { initAuth0 } from '@auth0/nextjs-auth0';
 
   const auth0 = initAuth0({
@@ -5,8 +13,8 @@ import { initAuth0 } from '@auth0/nextjs-auth0';
   clientId: 'WWV4kTpHy7f8583y2bNez2yRNI5ySL8Q',
   clientSecret: process.env.AUTH0_CLIENT_SECRET,
   scope: 'openid profile',
-  redirectUri: 'http://localhost:3000/api/v1/callback',
-  postLogoutRedirectUri: 'http://localhost:3000/',
+  redirectUri: redirectUrl,
+  postLogoutRedirectUri: postLogoutRedirectUrl,
   session: {
     // The secret used to encrypt the cookie.
     cookieSecret: process.env.AUTH0_COOKIE_SECRET,
