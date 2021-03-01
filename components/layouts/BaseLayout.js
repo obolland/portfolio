751d9b958e7abb9c29c0bf239a0a6d80e6eb2af3
OllyB <info@ollybolland.com>
2021-03-01 10:24:31 +0000

    .

--- components/layouts/BaseLayout.js
+++ components/layouts/BaseLayout.js
@@ -1,5 +1,4 @@
 import dynamic from 'next/dynamic'
-// import header from '../shared/Header';
 const Header = dynamic(() => import('../shared/Header'), {ssr: false});
 import { ToastContainer } from 'react-toastify';
 
@@ -9,7 +8,6 @@ const BaseLayout = ({ className, navClass="with-bg", children, user, userLoading
       {typeof window !== 'undefined' && (
         <Header className={navClass} user={user} userLoading={userLoading} />
       )}
-      {/* <Header className={navClass} user={user} userLoading={userLoading} /> */}
       <main className={`cover ${className}`}>
         <div className="wrapper">
           {children}
